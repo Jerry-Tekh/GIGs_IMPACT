0844b9ce27f49c64820ff606f88775015495d9a5
Jerry_tekh <jerry.chinonso134@gmail.com>
2026-05-20 15:06:25 +0100

    made the require change to support Aven database

--- gigBackend/src/config/db.js
+++ gigBackend/src/config/db.js
@@ -1,16 +1,30 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let pool;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+if (process.env.DATABASE_URL) {
+  // Production (Aiven / Render)
+  //using DATABASE_URL with SSL configuration for production environments 
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
+  });
+} else {
+  //  Local development
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 export default pool;
