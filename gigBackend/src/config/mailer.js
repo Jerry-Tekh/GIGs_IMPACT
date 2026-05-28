14d20c422df859e625be57ead8b72d65786409d8
Jerry_tekh <jerry.chinonso134@gmail.com>
2026-05-28 19:48:26 +0100

    working on the SMTP for email send fail duringn signup

--- gigBackend/src/config/mailer.js
+++ gigBackend/src/config/mailer.js
@@ -2,6 +2,10 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // App password
