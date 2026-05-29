863d55397cee13859cf8796cedf342893610d793
Jerry_tekh <jerry.chinonso134@gmail.com>
2026-05-29 12:11:35 +0100

    reberted back to nodemailer

--- gigBackend/src/config/mailer.js
+++ gigBackend/src/config/mailer.js
@@ -3,10 +3,7 @@ import nodemailer from 'nodemailer';
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  requireTLS: true,
-  logger:true,
-  debug:true,
-  secure: false,
+  secure: process.env.SMPT_SECURE,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // App password
