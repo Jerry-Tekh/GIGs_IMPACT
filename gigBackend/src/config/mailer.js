8c1704806711f8a388e5152df714a336f9d2d241
Jerry_tekh <jerry.chinonso134@gmail.com>
2026-05-29 18:35:56 +0100

    added the ipv settinf for mailer

--- gigBackend/src/config/mailer.js
+++ gigBackend/src/config/mailer.js
@@ -1,8 +1,11 @@
 import nodemailer from 'nodemailer';
+import dns from 'node:dns';
 import 'dotenv/config';
 
+dns.setDefaultResultOrder('ipv4first');
+
 const smtpPort = Number(process.env.SMTP_PORT || 587);
-const smtpSecureValue = process.env.SMTP_SECURE ?? process.env.SMTP_SECURE;
+const smtpSecureValue = process.env.SMTP_SECURE ?? process.env.SMPT_SECURE;
 const smtpSecure = smtpSecureValue
   ? String(smtpSecureValue).toLowerCase() === 'true'
   : smtpPort === 465;
@@ -11,15 +14,9 @@ export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: smtpPort,
   secure: smtpSecure,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // App password
   },
-});
-
-console.log({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE,
-  user: process.env.EMAIL_USER
 });
